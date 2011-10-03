386ad2f245ee277c51da8012217d38075181aa50
Joshua Krall <joshuakrall@pobox.com>
2011-10-03 04:30:20 -0500

    Add test merchant credentials

---
+++ samurai_credentials.php
@@ -0,0 +1,6 @@
+<?php
+  define( 'SAMURAI_PROCESSOR_TOKEN', '69ac9c704329bb067d427bf0' );
+  define( 'SAMURAI_MERCHANT_KEY', 'a1ebafb6da5238fb8a3ac9f6' );
+  define( 'SAMURAI_MERCHANT_PASSWORD', 'ae1aa640f6b735c4730fbb56' );
+  define( 'SAMURAI_LIB_DIRECTORY', dirname(__DIR__).'/lib' );
+?>
