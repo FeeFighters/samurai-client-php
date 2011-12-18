b9b9e55d65825fa4819a3c2b94ce2b2f9dc8931a
Joshua Krall <joshuakrall@pobox.com>
2011-12-18 00:40:39 -0600

    Oops, fixing broken test

--- test/Samurai/TransactionTest.php
+++ test/Samurai/TransactionTest.php
@@ -19,104 +19,104 @@ class Samurai_TransactionTest extends PHPUnit_Framework_TestCase
 	}
 
 	public function testCaptureShouldBeSuccessful() {
-	  $auth = Samurai::Processor.authorize($this->paymentMethod->token, 100.00);
-	  $transaction = $auth->capture();
+	  $transaction = Samurai_Processor::theProcessor()->authorize($this->paymentMethod->token, 100.00);
+    $transaction->capture();
 		$this->assertTrue( $transaction->isSuccess() );
 	}
 	public function testCaptureShouldBeSuccessfulForFullAmount() {
-	  $auth = Samurai::Processor.authorize($this->paymentMethod->token, 100.00);
-	  $transaction = $auth->capture(100.0);
+	  $transaction = Samurai_Processor::theProcessor()->authorize($this->paymentMethod->token, 100.00);
+	  $transaction->capture(100.0);
 		$this->assertTrue( $transaction->isSuccess() );
 	}
 	public function testCaptureShouldBeSuccessfulForPartialAmount() {
-	  $auth = Samurai::Processor.authorize($this->paymentMethod->token, 100.00);
-	  $transaction = $auth->capture(50.0);
+	  $transaction = Samurai_Processor::theProcessor()->authorize($this->paymentMethod->token, 100.00);
+	  $transaction->capture(50.0);
 		$this->assertTrue( $transaction->isSuccess() );
 	}
 
 	public function testCaptureFailuresShouldReturnProcessorTransactionInvalidWithDeclinedAuth() {
-	  $auth = Samurai::Processor.authorize($this->paymentMethod->token, 100.02);
-	  $transaction = $auth->capture();
+	  $transaction = Samurai_Processor::theProcessor()->authorize($this->paymentMethod->token, 100.02);
+	  $transaction->capture();
 		$this->assertFalse( $transaction->isSuccess() );
 		$this->assertEquals( 'This transaction type is not allowed.', $transaction->errors['processor.transaction'][0]->description );
 	}
 	public function testCaptureFailuresShouldReturnProcessorTransactionDeclined() {
-	  $auth = Samurai::Processor.authorize($this->paymentMethod->token, 100.00);
-	  $transaction = $auth->capture(100.02);
+	  $transaction = Samurai_Processor::theProcessor()->authorize($this->paymentMethod->token, 100.00);
+    $transaction->capture(100.02);
 		$this->assertFalse( $transaction->isSuccess() );
 		$this->assertEquals( 'The card was declined.', $transaction->errors['processor.transaction'][0]->description );
 	}
 	public function testCaptureFailuresShouldReturnInputAmountInvalid() {
-	  $auth = Samurai::Processor.authorize($this->paymentMethod->token, 100.00);
-	  $transaction = $auth->capture(100.10);
+	  $transaction = Samurai_Processor::theProcessor()->authorize($this->paymentMethod->token, 100.00);
+	  $transaction->capture(100.10);
 		$this->assertFalse( $transaction->isSuccess() );
 		$this->assertEquals( 'The transaction amount was invalid.', $transaction->errors['processor.transaction'][0]->description );
 	}
 
 	public function testReverseOnCaptureShouldBeSuccessful() {
-	  $purchase = Samurai::Processor.purchase($this->paymentMethod->token, 100.00);
-	  $transaction = $purchase->reverse();
+	  $transaction = Samurai_Processor::theProcessor()->purchase($this->paymentMethod->token, 100.00);
+    $transaction->reverse();
 		$this->assertTrue( $transaction->isSuccess() );
 	}
 	public function testReverseOnCaptureShouldBeSuccessfulForFullAmount() {
-	  $purchase = Samurai::Processor.purchase($this->paymentMethod->token, 100.00);
-	  $transaction = $purchase->reverse(100.00);
+	  $transaction = Samurai_Processor::theProcessor()->purchase($this->paymentMethod->token, 100.00);
+	  $transaction->reverse(100.00);
 		$this->assertTrue( $transaction->isSuccess() );
 	}
 	public function testReverseOnCaptureShouldBeSuccessfulForPartialAmount() {
-	  $purchase = Samurai::Processor.purchase($this->paymentMethod->token, 100.00);
-	  $transaction = $purchase->reverse(50.00);
+	  $transaction = Samurai_Processor::theProcessor()->purchase($this->paymentMethod->token, 100.00);
+	  $transaction->reverse(50.00);
 		$this->assertTrue( $transaction->isSuccess() );
 	}
 
 	public function testReverseOnAuthorizeShouldBeSuccessful() {
-	  $auth = Samurai::Processor.authorize($this->paymentMethod->token, 100.00);
-	  $transaction = $auth->reverse();
+	  $transaction = Samurai_Processor::theProcessor()->authorize($this->paymentMethod->token, 100.00);
+	  $transaction->reverse();
 		$this->assertTrue( $transaction->isSuccess() );
 	}
 	public function testReverseOnAuthorizefailuresShouldReturnInputAmountInvalid() {
-	  $auth = Samurai::Processor.authorize($this->paymentMethod->token, 100.00);
-	  $transaction = $auth->reverse(100.10);
+	  $transaction = Samurai_Processor::theProcessor()->authorize($this->paymentMethod->token, 100.00);
+	  $transaction->reverse(100.10);
 		$this->assertTrue( $transaction->isSuccess() );
 		$this->assertEquals( 'The transaction amount was invalid.', $transaction->errors['processor.transaction'][0]->description );
 	}
 
 	public function testCreditOnCaptureShouldBeSuccessful() {
-	  $purchase = Samurai::Processor.purchase($this->paymentMethod->token, 100.00);
-	  $transaction = $purchase->credit();
+	  $transaction = Samurai_Processor::theProcessor()->purchase($this->paymentMethod->token, 100.00);
+	  $transaction->credit();
 		$this->assertTrue( $transaction->isSuccess() );
 	}
 	public function testCreditOnCaptureShouldBeSuccessfulForFullAmount() {
-	  $purchase = Samurai::Processor.purchase($this->paymentMethod->token, 100.00);
-	  $transaction = $purchase->credit(100.00);
+	  $transaction = Samurai_Processor::theProcessor()->purchase($this->paymentMethod->token, 100.00);
+	  $transaction->credit(100.00);
 		$this->assertTrue( $transaction->isSuccess() );
 	}
 	public function testCreditOnCaptureShouldBeSuccessfulForPartialAmount() {
-	  $purchase = Samurai::Processor.purchase($this->paymentMethod->token, 100.00);
-	  $transaction = $purchase->credit(50.00);
+	  $transaction = Samurai_Processor::theProcessor()->purchase($this->paymentMethod->token, 100.00);
+	  $transaction->credit(50.00);
 		$this->assertTrue( $transaction->isSuccess() );
 	}
 
 	public function testCreditOnAuthorizeShouldBeSuccessful() {
-	  $auth = Samurai::Processor.authorize($this->paymentMethod->token, 100.00);
-	  $transaction = $auth->credit();
+	  $transaction = Samurai_Processor::theProcessor()->authorize($this->paymentMethod->token, 100.00);
+	  $transaction->credit();
 		$this->assertTrue( $transaction->isSuccess() );
 	}
 	public function testCreditOnAuthorizefailuresShouldReturnInputAmountInvalid() {
-	  $auth = Samurai::Processor.authorize($this->paymentMethod->token, 100.00);
-	  $transaction = $auth->credit(100.10);
+	  $transaction = Samurai_Processor::theProcessor()->authorize($this->paymentMethod->token, 100.00);
+	  $transaction->credit(100.10);
 		$this->assertTrue( $transaction->isSuccess() );
 		$this->assertEquals( 'The transaction amount was invalid.', $transaction->errors['processor.transaction'][0]->description );
 	}
 
 	public function testVoidOnAuthorizedShouldBeSuccessful() {
-	  $auth = Samurai::Processor.authorize($this->paymentMethod->token, 100.00);
-	  $transaction = $auth->void();
+	  $transaction = Samurai_Processor::theProcessor()->authorize($this->paymentMethod->token, 100.00);
+	  $transaction->void();
 		$this->assertTrue( $transaction->isSuccess() );
 	}
 	public function testVoidOnCapturedShouldBeSuccessful() {
-	  $purchase = Samurai::Processor.purchase($this->paymentMethod->token, 100.00);
-	  $transaction = $purchase->void();
+	  $transaction = Samurai_Processor::theProcessor()->purchase($this->paymentMethod->token, 100.00);
+	  $transaction->void();
 		$this->assertTrue( $transaction->isSuccess() );
 	}
 }
